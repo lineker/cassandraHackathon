de053dad2fbda4f6fb3225e7c64f1332b095eae1
lineker <lineket@gmail.com>
2013-11-23 22:43:36 -0500

    table by month

--- scripts/select_tests.sql
+++ scripts/select_tests.sql
@@ -12,3 +12,10 @@ WHERE location_id='room1'
 AND event_time > '2013-04-03 07:01:00'
 AND event_time < '2013-04-03 07:04:00'
 
+
+/*from feb - april*/
+SELECT *
+FROM temperature_by_month
+WHERE location_id='kitchen'
+AND event_time >= '2013-02-01 01:01:00'
+AND event_time < '2013-04-03 07:05:00'
