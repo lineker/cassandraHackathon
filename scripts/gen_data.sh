8cd7ca5c5128429c69211989a1e145fc8d173e8e
lineker <lineket@gmail.com>
2013-11-24 08:50:44 -0500

    new scripts

--- scripts/gen_data.sh
+++ scripts/gen_data.sh
@@ -2,18 +2,26 @@
 names=("kitchen" "room1" "living room" "bathroom")
 for i in {1..12}
 do
+	if (($i < 10));
+   	then
+   	 i="0$i"
+    fi 
 	for n in "${names[@]}"
 	do
-		 for a in {1..30}
+		for a in {1..30}
 		do
+		   if (($a < 10));
+		   	then
+		   	 a="0$a"
+		   fi
+		   #echo $i
 		   rand=$((RANDOM%100+0));	
-		   echo $rand 
-		   d="INSERT INTO energy_consumption_by_day(location_id,date,energy_consumption) VALUES ('$n','2013-$i-$a',$rand);"
-		   echo $d >> /Users/ltomazeli/cassandraHack/scripts/insert_energy_dataset.cql
+		   #echo $rand 
+		   echo "INSERT INTO energy_consumption_by_day(location_id,date,energy_consumption) VALUES ('$n','2013-$i-$a',$rand);"
 
 		   r=$((RANDOM%55+0));
-			t="INSERT INTO temperature_by_day(location_id,date,temperature) VALUES ('$n','2013-$i-$a 01:01:00',$r);"
-			echo $t >>/Users/ltomazeli/cassandraHack/scripts/insert_energy_dataset.cql
+		   echo "INSERT INTO temperature_by_day(location_id,date,temperature) VALUES ('$n','2013-$i-$a 01:01:00',$r);" 
+
 		done
 	done
   	 
@@ -24,11 +32,10 @@ do
 	for n in "${names[@]}"
 	do
 		rand=$((RANDOM%2000+1000));
-		m="INSERT INTO energy_consumption_by_month(location_id,event_time,energy_consumption) VALUES ('$n','2013-$i-01 01:01:00',$rand);"
-		echo $m >>/Users/ltomazeli/cassandraHack/scripts/insert_energy_dataset.cql
+		echo "INSERT INTO energy_consumption_by_month(location_id,event_time,energy_consumption) VALUES ('$n','2013-$i-01 01:01:00',$rand);"
 
 		r=$((RANDOM%55+0));
-		t="INSERT INTO temperature_by_month(location_id,event_time,temperature) VALUES ('$n','2013-$i-01 01:01:00',$r);"
-		echo $t >>/Users/ltomazeli/cassandraHack/scripts/insert_energy_dataset.cql
+		echo "INSERT INTO temperature_by_month(location_id,event_time,temperature) VALUES ('$n','2013-$i-01 01:01:00',$r);"
+
 	done
 done
